c29b7e6070418e440c9e329e03a890801323a00f
mahmoud fouad <mahmoud.ah.fouad@gmail.com>
2023-03-04 18:40:30 +0200

    all cross origin on http://localhost:5173

--- ecommerceBackend/src/main/java/com/mfouad/ecommerce/category/CategoryController.java
+++ ecommerceBackend/src/main/java/com/mfouad/ecommerce/category/CategoryController.java
@@ -2,6 +2,7 @@ package com.mfouad.ecommerce.category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("categories")
 public class CategoryController {
 
